1454bd6ee9cf382a07b2ff4ab8916a491839a6e7
AmalJAlappat <amaljalappatt@gmail.com>
2023-09-20 17:34:36 +0530

    Added Add-Doctor and Navbar Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddDoctorComponent } from './components/add-doctor/add-doctor.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddDoctorComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
