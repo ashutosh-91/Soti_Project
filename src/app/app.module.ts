10601b66c4ce8e6880a591c111cbc5441c7a4704
Ande <andeakash@gmail.com>
2023-09-20 18:42:18 +0530

    Add view-doctor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddDoctorComponent } from './components/add-doctor/add-doctor.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ViewDoctorComponent } from './components/view-doctor/view-doctor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddDoctorComponent,
-    NavbarComponent
+    NavbarComponent,
+    ViewDoctorComponent
   ],
   imports: [
     BrowserModule,
