00454e46d536b771fe41684aac1e375916b5262d
Aravind-Jose <aravindjosethottian@gmail.com>
2023-09-21 01:59:46 +0530

    Added the routing and updated various components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddDoctorComponent } from './components/add-doctor/add-doctor.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { UpdateSurgeryComponent } from './components/update-surgery/update-surgery.component';
 import { UpdateDoctorComponent } from './components/update-doctor/update-doctor.component';
+
 import { FormsModule,ReactiveFormsModule } from "@angular/forms";
 import { ViewDoctorComponent } from './components/view-doctor/view-doctor.component';
+import { ViewSpecializationComponent } from './components/view-specialization/view-specialization.component';
+import { ViewTodaysSurgeryComponent } from './components/view-todays-surgery/view-todays-surgery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddDoctorComponent,
     NavbarComponent,
-    UpdateDoctorComponent,
-    ViewDoctorComponent
+    UpdateSurgeryComponent,
+    ViewDoctorComponent,
+    ViewSpecializationComponent,
+    ViewTodaysSurgeryComponent,
+    UpdateDoctorComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/view-doctor/view-doctor.component.ts
+++ src/app/components/view-doctor/view-doctor.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IDoctor } from 'src/app/models/doctor.model';
 
 @Component({
   selector: 'app-view-doctor',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-doctor.component.css']
 })
 export class ViewDoctorComponent {
-
+  doctors:IDoctor[]=[{DoctorId: 1,DoctorName:"John"},{DoctorId: 2,DoctorName:"Alex"},{DoctorId: 3,DoctorName:"Ameer"}];
+  constructor( private router:Router){}
+  editDoctor(doctor:any){
+    this.router.navigate(['/doctor',doctor.DoctorId])
+  }
 }
