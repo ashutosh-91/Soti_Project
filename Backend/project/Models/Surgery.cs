c75f3de38dc73a54111522bb1cd4ada68021b8cf
Ashutosh <sashutosh479@gmail.com>
2023-09-24 01:03:32 +0530

    Added Validation To  add-doctor

--- Backend/project/Models/Surgery.cs
+++ Backend/project/Models/Surgery.cs
@@ -8,6 +8,8 @@ namespace project.Models
        public int? DoctorId { get; set; }
        public decimal EndTime { get; set; }
        public decimal StartTime { get; set; }
+
+
         public int SurgeryId { get; set; }
         public string SurgeryCategory { get; set; }
         public DateTime SurgeryDate { get; set; }
